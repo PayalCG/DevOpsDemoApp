a1fd7f476b2444f577ca37adc96f517afa0b42bd
Payal <payal.pl@capgemini.com>
2026-07-27 14:57:41 +0530

    Added new workflow for test with fastlane

---
+++ DevOpsDemoAppTests/DevOpsDemoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  DevOpsDemoAppTests.swift
+//  DevOpsDemoAppTests
+//
+//  Created by Payal on 27/07/26.
+//
+
+import Testing
+
+struct DevOpsDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
