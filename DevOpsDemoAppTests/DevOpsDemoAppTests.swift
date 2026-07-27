df911135a017070f959fbe34e059759e920ee9b0
Payal <payal.pl@capgemini.com>
2026-07-27 15:08:39 +0530

    Added new workflow for failed test with fastlane

--- DevOpsDemoAppTests/DevOpsDemoAppTests.swift
+++ DevOpsDemoAppTests/DevOpsDemoAppTests.swift
@@ -10,7 +10,7 @@ import Testing
 struct DevOpsDemoAppTests {
 
     @Test func example() async throws {
-        #expect(2*2 == 4)
+        #expect(2*2 == 6)
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
