150f74c1c3af69bcf1d091446e48d12428116014
Payal <payal.pl@capgemini.com>
2026-07-28 23:28:39 +0530

    Created new branch login

--- DevOpsDemoApp/ContentView.swift
+++ DevOpsDemoApp/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
             List {
                 ForEach(1..<11) { index in
                     Text("Row number \(index)")
+                    Image("star")
+                        .resizable()
+                        .scaledToFit()
                 }
             }
         }
